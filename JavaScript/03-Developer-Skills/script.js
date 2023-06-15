87c05848b00f03c3fe8fbb49a9a7c562821aac54
“samiul-gmu” <sislam22@masonlive.gmu.edu>
2023-06-15 12:25:47 -0400

    Finished lecture 59

--- JavaScript/03-Developer-Skills/script.js
+++ JavaScript/03-Developer-Skills/script.js
@@ -19,3 +19,46 @@ console.log(calcAge(1995));
 // 2. Divide and conquer (break it into sub-problems)
 // 3. Don't be afraid to research to find an optimal solution
 // 4. Start with the pseudo code before jumping in to code it
+
+// 59 - Using Google StackOverflow and MDN
+const temperatures = [-3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+// 1. Understand the problem
+// What is tempp amplitude? Ans: Diff between the highest and lowest temp.
+// How to compute the max and min temp from an array of values?
+// What is a sensor error? And what to do with them?
+
+// 2. Divide and conquer
+// Ignore errors
+// Find the max value?
+// Find the min value?
+// Subtract min from max and return it
+
+const calcTempAmplitude = function (temps) {
+  let max;
+  let min;
+  let defined = false;
+  for (let i = 0; i < temps.length; i++) {
+    if (defined === true) {
+      if (max < temps[i]) max = temps[i];
+      if (min > temps[i]) min = temps[i];
+    } else {
+      if (typeof temps[i] === 'number') {
+        max = min = temps[i];
+        defined = true;
+      }
+    }
+  }
+  if (defined) return max - min;
+  else return 'undefined';
+};
+
+console.log(calcTempAmplitude(temperatures));
+
+// now update the method so that it can take two arrays and concat them before finding the temp amplitude
+const calcTempAmplitude2 = function (temps1, temps2) {
+  const mergedArray = temps1.concat(temps2);
+  return calcTempAmplitude(mergedArray);
+};
+
+const temperatures2 = [10, 20, 30, 40, 50];
+console.log(calcTempAmplitude2(temperatures, temperatures2));
