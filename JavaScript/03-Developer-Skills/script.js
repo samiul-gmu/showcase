cd5db2e4d6d4310093d69868ab771854a72b642a
“samiul-gmu” <sislam22@masonlive.gmu.edu>
2023-06-15 11:53:46 -0400

    Finished lecture 57

--- JavaScript/03-Developer-Skills/script.js
+++ JavaScript/03-Developer-Skills/script.js
@@ -1,8 +1,15 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+/*
+// 55 - Setting up Prettier and VS Code
+// 56 - Installing Nodejs and Setting Up a Dev Environment
 const x = 23;
 if (x === 23) console.log(23);
 
 const calcAge = birthYear => 2037 - birthYear;
 console.log(calcAge(1995));
+*/
+
+// 57 - Learning How to Code
+// Think of a big project where you can showcase all of your skills
