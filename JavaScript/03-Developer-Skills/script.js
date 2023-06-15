16602199155a30e81e2f3857356e5610fa298d79
“samiul-gmu” <sislam22@masonlive.gmu.edu>
2023-06-15 12:00:56 -0400

    Finished lecture 58

--- JavaScript/03-Developer-Skills/script.js
+++ JavaScript/03-Developer-Skills/script.js
@@ -9,7 +9,13 @@ if (x === 23) console.log(23);
 
 const calcAge = birthYear => 2037 - birthYear;
 console.log(calcAge(1995));
-*/
 
 // 57 - Learning How to Code
 // Think of a big project where you can showcase all of your skills
+*/
+
+// 58 - How to Think Like a Developer Become a Problem Solver
+// 1. Understand the problem (ask the right questions to understand it fully)
+// 2. Divide and conquer (break it into sub-problems)
+// 3. Don't be afraid to research to find an optimal solution
+// 4. Start with the pseudo code before jumping in to code it
