7e303761d98762d0fb770baddf82ee03674822a6
“samiul-gmu” <sislam22@masonlive.gmu.edu>
2023-06-16 16:13:04 -0400

    Finished lecture 71

--- JavaScript/05-Guess-My-Number/script.js
+++ JavaScript/05-Guess-My-Number/script.js
@@ -1,3 +1,8 @@
 'use strict';
-
+/*
+// 70 - PROJECT 1 Guess My Number
 console.log(document.querySelector('.message').textContent);
+*/
+
+// 71 - Whats the DOM and DOM Manipulation
+// DOM: Document Object Model
