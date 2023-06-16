35f260ee27ca10951e1fadff9b2b0a53882b2020
“samiul-gmu” <sislam22@masonlive.gmu.edu>
2023-06-16 18:01:46 -0400

    Finished lecture 72

--- JavaScript/05-Guess-My-Number/script.js
+++ JavaScript/05-Guess-My-Number/script.js
@@ -1,8 +1,15 @@
 'use strict';
-/*
+
 // 70 - PROJECT 1 Guess My Number
 console.log(document.querySelector('.message').textContent);
-*/
 
 // 71 - Whats the DOM and DOM Manipulation
 // DOM: Document Object Model
+
+// 72 - Selecting and Manipulating Elements
+document.querySelector('.message').textContent = 'Correct Number!';
+document.querySelector('.number').textContent = 3;
+document.querySelector('.score').textContent = 17;
+
+document.querySelector('.guess').value = 15;
+console.log(document.querySelector('.guess').value);
