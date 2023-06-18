9a7211b8ffd6533021d5f2687f5cb507667d2281
“samiul-gmu” <sislam22@masonlive.gmu.edu>
2023-06-18 14:05:01 -0400

    Finished lecture 97

--- JavaScript/08-Behind-the-Scenes/script.js
+++ JavaScript/08-Behind-the-Scenes/script.js
@@ -22,7 +22,7 @@ function calc(birthYear) {
 
 const firstName = 'Sami';
 console.log(calc(1992));
-*/
+
 
 // 95 - Hoisting and TDZ in Practice
 // Variables
@@ -68,3 +68,49 @@ const z = 3;
 console.log(x === window.x);
 console.log(y === window.y);
 console.log(z === window.z);
+
+// 97 - The this Keyword in Practice
+// this keyword will point to the window of the global scope
+console.log(this);
+
+// this keyword here will be undefined as regular function does not get the global object in strict mode
+const calcAge = function (birthYear) {
+  console.log(2023 - birthYear);
+  console.log(this);
+};
+
+calcAge(1992);
+
+// this keyword here will get the global window as the arrow function gets its lexical parent
+const calcArrow = birthYear => {
+  console.log(2023 - birthYear);
+  console.log(this);
+};
+
+calcArrow(1992);
+
+// this keyword used inside a function that is part of an object will return that object
+const jonas = {
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+    console.log(2023 - this.year);
+  },
+};
+
+jonas.calcAge();
+
+// An example, where we are borrowing a method from another class to another. this keyword is actually point to the object on which it gets invoked.
+const matilda = {
+  year: 2017,
+};
+
+matilda.calcAge = jonas.calcAge;
+console.log(matilda);
+matilda.calcAge();
+
+// copying the function to a variable
+const f = jonas.calcAge;
+
+//f(); // this will produce an error as the this keyword here is undefined as we are not calling the function on an object
+*/
