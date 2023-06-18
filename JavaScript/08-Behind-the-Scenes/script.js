d8404198091014079a6aa2f294edcf79f2b00a51
“samiul-gmu” <sislam22@masonlive.gmu.edu>
2023-06-18 12:22:03 -0400

    Finished lecture 93

--- JavaScript/08-Behind-the-Scenes/script.js
+++ JavaScript/08-Behind-the-Scenes/script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+function calc(birthYear) {
+  const age = 2023 - birthYear;
+
+  function printAge() {
+    const output = `${firstName} is ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      const str = `Oh, and I'm a millenial!`;
+      console.log(str);
+    }
+    console.log(millenial);
+  }
+  printAge();
+  return age;
+}
+
+const firstName = 'Sami';
+console.log(calc(1992));
