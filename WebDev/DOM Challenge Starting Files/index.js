2adcf1fee5e0e5bb58a61024549b7adffadacd56
“samiul-gmu” <sislam22@masonlive.gmu.edu>
2023-07-08 08:34:01 -0400

    WebDev DOM

---
+++ WebDev/DOM Challenge Starting Files/index.js
@@ -0,0 +1,17 @@
+// document.getElementsByTagName("li")[2].innerHTML = "SAMIUL";
+
+// document.getElementsByClassName("btn")[0].style.color = "blue";
+
+// document.getElementById("title").innerHTML = "GOOD BYE";
+// document.getElementById("title").style.color = "BROWN";
+
+// document.querySelector(".list a").style.color = "red";
+// document.getElementsByClassName("btn")[0].style.backgroundColor = "yellow";
+
+document.querySelector(".btn").classList.toggle("huge");
+
+document.querySelector("h1").textContent = "TEXT CONTENT";
+
+// document.querySelector("a").getAttribute("href");
+
+document.querySelector("a").setAttribute("href", "https://www.gmail.com");
